a1c954c7b3001e962f4a114d0674d58f1a82ec68
Jose Del Valle <jdelvalle88@live.com>
2023-08-08 16:41:57 -0400

    changed logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -9,13 +9,13 @@ const Logo = () => {
         {/* Add the SVG arc here */}
         <svg className="arc" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
           {/* The arc path goes from A to Z, passing over "uri" */}
-          <path d="M 10 90 Q 30 -20 50 10 Q 70 -20 90 50 Q 10 20 130 10 Q 15 20 17 100" fill="transparent" stroke="aqua" stroke-width="10" />
+          {/* <path d="M 10 90 Q 30 30 50 10 Q 10 20 180 10 Q 15 20 17 100" fill="transparent" stroke="aqua" stroke-width="20" /> */}
         </svg>
         {/* Display the letters A and Z */}
-        <span>A</span>uri<span>z</span>on
+        {/* <span>A</span>uri<span>z</span>on */}
       </div>
       <Link className="no-underline flex items-center" to="/">
-        {/* <span className="bg-aqua text-black bold px-1">A</span>urizon */}
+        <span className="logo-span bg-aqua text-black bold px-1">A</span>urizon
       </Link>
     </div>
   );
