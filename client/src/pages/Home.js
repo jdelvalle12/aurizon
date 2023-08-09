d5edb76627e47cf23ceebd7eb5ab6503d0e0a76d
Jose Del Valle <jdelvalle88@live.com>
2023-08-09 16:55:32 -0400

    a little more added to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,23 +7,36 @@ const Home = () => {
 
   useEffect(() => {
     setShowSlogan(true);
+
+    const typingText = 'Your gateway to extraordinary solutions';
+    const typingSpeed = 50; // Speed in milliseconds between each character
+
+    const typingContainer = document.getElementById('typing-text');
+    let charIndex = 0;
+
+    function typeText() {
+      if (charIndex < typingText.length) {
+        typingContainer.textContent += typingText.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, typingSpeed);
+      }
+    }
+
+    typeText();
   }, []);
 
   return (
     <div className="home-content">
-      {/* Add the "fade-in" class if showSlogan is true */}
       <h1 className={showSlogan ? 'fade-in' : ''}>"Aeon Innovations, Illuminating Tomorrow"</h1>
-      <h2>Your gateway to extraordinary solutions</h2>
+      <h2>
+        <span id="typing-text"></span>
+      </h2>
+      <p className={showSlogan ? 'slide-in' : ''}>
+        At Aurizon, we build technology that helps businesses succeed and develop intuitive and user-friendly products
+        to simplify everyday tasks.
+      </p>
     </div>
   );
 };
 
 export default Home;
-
-
-
-
-
-
-
-
