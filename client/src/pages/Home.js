dccf7aba8fa56dc06015e44335f72dd4ab81018b
Jose Del Valle <jdelvalle88@live.com>
2023-09-01 00:06:08 -0400

    added more content on homepage, including glass effect boxes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Motion from '../images/background-motion.mp4'
+import Motion from '../images/background-motion.mp4';
+// import Coding from '../images/coding.mp4';
 import '../index.css';
 import '../App.css';
 
@@ -28,25 +29,70 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-content">
+    <div className="home-container">
       <video src={Motion} autoPlay loop muted className="background-video"></video>
       <h1 className={showSlogan ? 'fade-in' : ''}>"Aeon Innovations, Illuminating Tomorrow"</h1>
       <h2>
         <span id="typing-text"></span>
       </h2>
-        <span className="mission">
-          We embark on a journey of innovation and on a mission
-          to be at the forefront of technological advancements
+        <span className={`mission ${showSlogan ? 'fade-in' : ''}`}>
+        Embarking on an innovation journey, we lead with purpose,
+        guiding progress towards a brighter future. Committed to 
+        pioneering technology, we empower businesses and individuals 
+        through cutting-edge solutions.
         </span>
       <div className={`button-container ${showSlogan ? 'slide-in' : ''}`}>
         <Link to="/contact" className={`contact-button ${showSlogan ? 'slide-in' : ''}`}>Let's Get Started</Link>
       </div>
+      <div>
         <h3>What We Do</h3>
         <p className={showSlogan ? 'slide-in' : ''}>
-          At Aurizon, we build technology that helps businesses succeed and develop intuitive and user-friendly products
+          At Aurizon, we build and manage technology that helps businesses succeed and develop intuitive and user-friendly products
           to simplify everyday tasks.
-        </p>
-      
+        </p>      
+        {/* <video src={Coding} autoPlay loop muted className="motion-video"></video> */}
+        <div className="service-container">
+          <div className="service-box web">
+            <h4>Web Development</h4>
+              <p>Crafting user-focused websites with stunning designs and 
+                seamless functionality, transforming ideas into engaging 
+                online experiences.
+              </p>
+          </div>
+          <div className="service-box api">
+            <h4>API Development</h4>
+              <p>Building powerful APIs that connect applications and services,
+                enabling smooth data exchange and enhancing software interoperability.
+              </p>
+          </div>
+          <div className="service-box business">
+            <h4>Business Solution</h4>
+              <p>Creating tailored software solutions that streamline business processes,
+                drive efficiency, and empower organizations to thrive.
+              </p>
+          </div>
+          <div className="service-box manage">
+            <h4>Web Management</h4>
+              <p>
+                Efficiently overseeing the operation, maintenance, and optimization of websites
+                to ensure seamless performance, security, and a flawless user experience.
+              </p>
+          </div>
+          <div className="service-box manage">
+            <h4>Consult & Strategy</h4>
+              <p>
+              We offer technology consulting services to help clients define their digital strategies,
+              align technology solutions with business goals, and plan for future growth.
+              </p>
+          </div>
+          <div className="service-box mobile">
+            <h4>Mobile App Developemnt</h4>
+              <p>Coming soon: Expertly crafting mobile applications that bring your ideas 
+                to life on smartphones and tablets, delivering unique user experiences.
+              </p>
+          </div>
+        </div>
+      </div>     
     </div>
   );
 };
