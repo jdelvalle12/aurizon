580b1fd42c0507dc21d73da4c62cc0472de385d9
Jose Del Valle <jdelvalle88@live.com>
2023-12-16 21:27:50 -0500

    getting started with careers page and added a background image. Next is to fill the page with contents.

--- client/src/pages/Careers.js
+++ client/src/pages/Careers.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import emailjs from 'emailjs-com'; // Import email.js library
+import CareersBackground from '../images/careers-background.jpg'
 
 import '../index.css';
 import '../App.css';
 
 const Careers = () => {
+    const [showCareerTitle, setCareerTitle] = useState(false);
+
+    useEffect(() => {
+        setCareerTitle(true);
+
+        const typingText = "The future starts with you";
+        const typingSpeed = 30;
+
+        const typingContainer = document.getElementById('typing-text');
+        let charIndex = 0;
+
+        function typeText() {
+            if(charIndex < typingText.length) {
+                typingContainer.textContent += typingText.charAt(charIndex);
+                charIndex++;
+                setTimeout(typeText, typingSpeed);
+            }
+        }
+        typeText();
+    }, []);
 
     
 return (
-        <div className='careers-container'>
+    <div className='careers-container'>
+      <img src={CareersBackground} alt='CareersBackground' className='careers-background'></img>
+        <div className={`career-title ${showCareerTitle ? 'slide-in' : ''}`} >
+            <h2>Careers</h2>
+        </div>
+        <div className='head-text'>
+            <p id='typing-text'></p>
+        </div>
            <p>At Aurizon, we're fueled by ambitious goals and innovative ideas,
            and we recognize that achieving them requires a dedicated and talented
            team. We invite individuals from diverse backgrounds, whether you're a
