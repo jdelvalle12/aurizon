994a323b94d95353eeee1b537b2afa5c17fd17d4
Jose Del Valle <jdelvalle88@live.com>
2024-05-10 23:40:22 -0400

    added react hook form for the contact page, but receving errors. Will review how to fix the errors and check what might need to change.

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 // import emailjs from 'emailjs-com'; // Import email.js library
 import ContactUs from '../images/contact-us-two.jpg';
 import '../styles/Contact.css';
 import '../App.css';
 
 const Contact = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   // const sendEmail = (e) => {
   //   e.preventDefault();
   //   emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
@@ -18,36 +25,54 @@ const Contact = () => {
   // };
 
   return (
-    <section>
-      <img src={ContactUs} alt='ContactUs' className="contact-us-two"></img>
       <div className='contact-container'>
+      <img src={ContactUs} alt='ContactUs' className="contact-us-background"></img>
         <h1 className='contact-title'>Contact</h1>
         <div className="design-inquiries">
           <h3 className='contact-header'>Design Inquiries:</h3>
-            <p> Do you have a question or comment? Click on the button below and provide the following:</p>
+            <p>Do you have a question? Click on the button below and provide the following:</p>
               <ul>
                 <li>Name</li>
                 <li>Email</li>
                 <li>Nature of your inquiry</li>
                 <li>Email to: <button onClick={() => window.location.href = 'mailto:jdelvalle88@live.com'}>jdelvalle88@live.com</button></li>
               </ul>
-          {/* <form onSubmit={sendEmail}> 
-            <label className="text-right" htmlFor="name">Name:</label>
-            <input type="text" id="name" name="from_name" /> 
-            <br />
-            <label className="text-right" htmlFor="email">Email:</label>
-            <input type="text" id="email" name="from_email" /> 
-            <br />
-            <label className="text-right" htmlFor="message">Message:</label>
-            <input type="text" id="message" name="message" /> 
-            <br />
-            <div className="text-center">
-              <button type="submit">SEND</button>
-            </div>
-          </form> */}
+           <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor='firstName'>First Name:</label>
+            <input type='text' name='firstName' id='firstName' ref={register({required: true})} />
+            {errors.firstName && <span>This field is required</span>}
+
+            <label htmlFor='lastName'>Last Name:</label>
+            <input type='text' name='lastName' id='lastName' ref={register({required: true})} />
+            {errors.lastName && <span>This field is required</span>}
+
+            <label htmlFor='companyEmail'>Company Email:</label>
+            <input type='email' name='companyEmail' id='companyEmail' ref={register({required: true})} />
+            {errors.companyEmail && <span>This field is required</span>}
+
+            <label htmlFor='phone'>Phone:</label>
+            <input type='text' name='phone' id='phone' ref={register({required: true})} />
+            {errors.phone && <span>This field is required</span>}
+
+            <label htmlFor='companyName'>Company Name:</label>
+            <input type='text' name='companyName' id='companyName' ref={register} />
+
+            <lable htmlFor='industry'>Industry:</lable>
+            <input type='text' name='industry' id='industry' ref={register} />
+
+            <label htmlFor='state'>State:</label>
+            <input type='text' name='state' id='state' ref={register} />
+
+            <label htmlFor='intent'>Intent of Message:</label>
+            <input type='text' name='intent' id='intent' ref ={register} />
+
+            <label htmlFor='message'>Message:</label>
+            <textarea name='message' id='message' ref={register} />
+
+            <input type='submit' value='Submit' />
+           </form>
         </div>
       </div>
-    </section>
   );
 }
 
